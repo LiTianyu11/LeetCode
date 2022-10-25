b19c235f8fe2c7912621a40444e48f0d4bf72e32
LiTianyu11 <96253991+LiTianyu11@users.noreply.github.com>
2022-10-25 22:30:09 +0800

    Change

--- 🟢Easy/demo.js
+++ 🟢Easy/demo.js
@@ -1,26 +1,4 @@
-var isIsomorphic = function (s, t) {
-  
+let arr = [0]
+arr.push(2)
 
-  const map1 = [256];
-  const map2 = [256];
-
-  for (let idx = 0; idx < s.length; idx++) {
-    console.log("----------------");
-    // console.log(
-    //   `map1 : ${map1}, s.charAt(idx): ${s.charAt(idx)}, map1[s.charAt(idx)] : ${
-    //     map1[s.charAt(idx)]
-    //   }`
-    // );
-    console.log(map1, s.charAt(idx),map1[s.charAt(idx)] )
-    map1[s.charAt(idx)] = idx + 1;
-    console.log(map1, s.charAt(idx),map1[s.charAt(idx)] )
-    // console.log(
-    //   `map1 : ${map1}, s.charAt(idx): ${s.charAt(idx)}, map1[s.charAt(idx)] : ${
-    //     map1[s.charAt(idx)]
-    //   }`
-    // );
-  }
-  return true;
-};
-
-isIsomorphic("aba", "abcdd");
+console.log(arr );
