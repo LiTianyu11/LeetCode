edc360a4edf5dc7ead83fa51fbb6c2b53e8630fe
LiTianyu11 <96253991+LiTianyu11@users.noreply.github.com>
2022-10-28 21:56:31 +0800

    Create 203. Remove Linked List Elements.js

---
+++ 🟢Easy/203. Remove Linked List Elements.js
@@ -0,0 +1,37 @@
+/*203. 
+Remove Linked List ElementsGiven the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+
+*/
+
+
+//Solution 1
+var removeElements = function(head, val) {
+    const dummyHead = new ListNode(0);
+    dummyHead.next = head;
+    let temp = dummyHead;
+    while (temp.next !== null) {
+        if (temp.next.val == val) {
+            temp.next = temp.next.next;
+        } else {
+            temp = temp.next;
+        }
+    }
+    return dummyHead.next;
+};
+
