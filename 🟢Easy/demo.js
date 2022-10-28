c664309721189506978067c4a941cc15bc608a2d
LiTianyu11 <96253991+LiTianyu11@users.noreply.github.com>
2022-10-28 20:14:11 +0800

    Update demo.js

--- 🟢Easy/demo.js
+++ 🟢Easy/demo.js
@@ -1,2 +1,62 @@
-let res = new Array(3).fill(0).map(()=> new Array(3).fill(0))
-console.log(res);
+// const list = {
+//     head: {   // head = node1
+//         value: 6
+//         next: {
+//             value: 10
+//             next: {
+//                 value: 12
+//                 next: {
+//                     value: 3
+//                     next: null
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// };
+
+class ListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+// linkedlist class
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  add(data) {
+    // creates a new node
+    var node = new ListNode(data);
+
+    // to store current node
+    var current;
+
+    // if list is Empty add the
+    // element and make it head
+    if (this.head == null) this.head = node;
+    else {
+      current = this.head;
+
+      // iterate to the end of the
+      // list
+      while (current.next) {
+        current = current.next;
+      }
+
+      // add node
+      current.next = node;
+    }
+    this.size++;
+  }
+}
+
+let list = new LinkedList();
+list.add(3);
+list.add(5)
+list.add(6)
+
