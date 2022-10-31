3573b44d50acd197bcd655414e066e5f43f6fac4
LiTianyu11 <96253991+LiTianyu11@users.noreply.github.com>
2022-10-31 21:34:30 +0800

    Create 24. Swap Nodes in Pairs.js

---
+++ 🟡Middle/24. Swap Nodes in Pairs.js
@@ -0,0 +1,40 @@
+/*24. Swap Nodes in Pairs
+
+Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+ 
+
+Example 1:
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+Example 2:
+
+
+Example 2:
+Input: head = []
+Output: []
+Example 3:
+
+
+Example 3:
+Input: head = [1]
+Output: [1]
+ 
+*/
+
+// Soulution 1
+var swapPairs = function (head) {
+  let dummyHead = new ListNode(0, head);
+  let cur = dummyHead;
+  let temp = null,
+    temp1 = null;
+  while (cur.next && cur.next.next) {
+    temp = cur.next;
+    temp1 = cur.next.next.next;
+    cur.next = cur.next.next;
+    cur.next.next = temp;
+    temp.next = temp1;
+    cur = cur.next.next;
+  }
+  return dummyHead.next;
+};
