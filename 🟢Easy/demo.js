854ea79fafb833278528623058bfa974c6adee4e
LiTianyu11 <96253991+LiTianyu11@users.noreply.github.com>
2022-11-09 15:11:39 +0800

    Update demo.js

--- 🟢Easy/demo.js
+++ 🟢Easy/demo.js
@@ -1,3 +1,25 @@
-let s= Array(5);
-s[5] = 0
-console.log(s);
+var replaceSpace = function (s) {
+    const arr = Array.from(s);
+    console.log(arr);
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === ' ') { count++; }
+    }
+    console.log(count);
+    let f = arr.length + count * 2 - 1;
+    console.log(f);
+    for (let i = arr.length - 1; i >= 0; i--) {
+        console.log(arr);
+        if (arr[i] === ' ') {
+            arr[f--] = '0';
+            arr[f--] = '2';
+            arr[f--] = '%';
+        } else {
+            arr[f] = arr[i];
+            f--;
+        }
+    }
+    return arr.join('')
+};
+
+replaceSpace("We are happy.")
